5484ee4806810a84e8fb451426a977cacc201fb8
Natnael Getachew <87979161+natnaeln4d@users.noreply.github.com>
2024-09-21 16:33:35 +0300

    category flitter

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -25,6 +25,7 @@
 
                                     @include('admin.categories.create')
 
+
                                 </div>
                             </div>
                         </div>
