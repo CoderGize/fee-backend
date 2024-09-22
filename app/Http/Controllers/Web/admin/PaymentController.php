99b6580ba31840cc4a62b96923186ffc865ac1fa
Natnael Getachew <87979161+natnaeln4d@users.noreply.github.com>
2024-09-22 12:16:39 +0300

    payment flitter and search

--- app/Http/Controllers/Web/admin/PaymentController.php
+++ app/Http/Controllers/Web/admin/PaymentController.php
@@ -8,10 +8,27 @@ use Illuminate\Http\Request;
 
 class PaymentController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
 
-        $payments = Payment::with(['user', 'order'])->paginate(10);
+        $search = $request->input('search');
+        $status = $request->input('status');
+        $paymentMethod = $request->input('payment_method');
+        $perPage = $request->input('per_page', 10);
+
+        $payments = Payment::with(['user', 'order'])
+                ->when($search, function ($query, $search) {
+                    return $query->whereHas('user', function ($q) use ($search) {
+                        $q->where('f_name', 'like', '%' . $search . '%');
+                    });
+                })
+                ->when($status, function ($query, $status) {
+                    return $query->where('status', $status);
+                })
+                ->when($paymentMethod, function ($query, $paymentMethod) {
+                    return $query->where('payment_method', $paymentMethod);
+                })
+                ->paginate($perPage);
 
 
         return view('admin.payments.index', compact('payments'));
