e737078c8f7bad30bc6774c585c330a5dc964c5f
Natnael Getachew <87979161+natnaeln4d@users.noreply.github.com>
2024-09-27 19:05:58 +0300

    nullable on the order and payment

--- database/migrations/2024_07_12_164929_create_payments_table.php
+++ database/migrations/2024_07_12_164929_create_payments_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('payments', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->nullable()->constrained()->onDelete('cascade');
             $table->foreignId('order_id')->constrained()->onDelete('cascade');
             $table->decimal('amount', 10, 2);
             $table->string('status')->default('pending');
