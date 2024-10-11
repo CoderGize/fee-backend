839378db7e66d6d9d5657b999a61dd63426f426b
Natnael Getachew <87979161+natnaeln4d@users.noreply.github.com>
2024-10-11 10:49:19 +0300

    phone digit lentgth added extra

--- app/Http/Controllers/Api/Auth/AuthUserController.php
+++ app/Http/Controllers/Api/Auth/AuthUserController.php
@@ -27,7 +27,7 @@ class AuthUserController extends Controller
                 'password' => 'required|string|min:8|confirmed',
                 'address' => 'nullable|string',
                 'city' => 'nullable|string|max:255',
-                'phone_number' => 'nullable|numeric|digits_between:10,20',
+                'phone_number' => 'nullable|numeric|digits_between:10,25',
             ]);
 
             if ($validator->fails()) {
