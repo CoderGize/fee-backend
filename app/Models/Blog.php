848bdae00ecba9062c8f6e5c2e5fa4f2395f710f
Natnael Getachew <87979161+natnaeln4d@users.noreply.github.com>
2024-10-13 16:37:36 +0300

    blog cast to json

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -8,4 +8,26 @@ use Illuminate\Database\Eloquent\Model;
 class Blog extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'img',
+        'profile',
+        'link',
+        'name_en',
+        'name_ar',
+        'title_en',
+        'title_ar',
+        'date',
+        'content_ar_1',
+        'content_en_1',
+        'content_ar_2',
+        'content_en_2',
+        'sub_title_ar',
+        'sub_title_en',
+        'blog_images',
+    ];
+
+    protected $casts = [
+        'blog_images' => 'arrays',
+    ];
 }
